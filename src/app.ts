74fde48823c43b0cbe15c93df69c44dd2c1081d7
yuki fujita <yfujita@yukinoMacBook-Pro.local>
2021-10-31 14:48:34 +0900

    apply typescript

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
-import  {Server, IncomingMessage, ServerResponse, createServer} from "http"
+import express = require("express");
+const app: express.Express = express();
+const port: string = process.env.PORT || "3000";
+
+type Users = {
+  id: string;
+  name: string;
+};
+
+const users = [
+  { id: "01", name: "hoge1" },
+  { id: "02", name: "fuga2" },
+  { id: "03", name: "piyo3" },
+];
 
 class Main {
-    constructor() {
-        const server: Server = createServer(
-            (request: IncomingMessage, response: ServerResponse) => this.requestHandler(request, response)
-        );
-        server.listen("5000");
-        console.log("http://localhost:5000 へアクセスください");
-    }
-
-    private requestHandler(request: IncomingMessage, response: ServerResponse) :void {
-        response.end("hello! NodeJs!!");
-    }
+  constructor() {
+    app.get("/", (req: express.Request, res: express.Response) => {
+      res.send("Simple REST API");
+    });
+    app.get("/api/users", (req: express.Request, res: express.Response) => {
+      res.send(users);
+    });
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+  }
 }
 
 const main = new Main();
